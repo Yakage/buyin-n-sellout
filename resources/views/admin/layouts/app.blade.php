4612705f68954464d20ba7848cf569007dbed55a
Mark Benedict Abalos <benedictabalos30@gmail.com>
2024-02-17 13:32:45 +0800

    updated

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,17 +5,17 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Laravel Shop :: Administrative Panel</title>
 		<!-- Google Font: Source Sans Pro -->
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 		<!-- Font Awesome -->
-		<link rel="stylesheet" href="{{ secure_asset('admin_assets/plugins/fontawesome-free/css/all.min.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ secure_asset('admin_assets/plugins/fontawesome-free/css/all.min.css') }}">
 		<!-- Theme style -->
-		<link rel="stylesheet" href="{{ secure_asset('admin_assets/css/adminlte.min.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ secure_asset('admin_assets/css/adminlte.min.css') }}">
 
-		<link rel="stylesheet" href="{{ secure_asset('admin_assets/plugins/dropzone/min/dropzone.min.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ secure_asset('admin_assets/plugins/dropzone/min/dropzone.min.css') }}">
 
-		<link rel="stylesheet" href="{{ secure_asset('admin_assets/plugins/summernote/summernote.min.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ secure_asset('admin_assets/plugins/summernote/summernote.min.css') }}">
 
-		<link rel="stylesheet" href="{{ secure_asset('admin_assets/css/custom.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ secure_asset('admin_assets/css/custom.css') }}">
 		
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 	</head>
