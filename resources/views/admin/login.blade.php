244fb4c8d271b563ecc139d5d87a76a03ed9e701
Mark Benedict Abalos <benedictabalos30@gmail.com>
2024-02-17 13:41:45 +0800

    updated

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -9,7 +9,7 @@
 		<!-- Font Awesome -->
 		<link rel="stylesheet" href=" {{ secure_asset('admin_assets/plugins/fontawesome-free/css/all.min.css') }}">
 		<!-- Theme style -->
-		<link rel="stylesheet" href=" {{ secure_asset('admin_assets/css/adminlte.min.css') }}">4
+		<link rel="stylesheet" href=" {{ secure_asset('admin_assets/css/adminlte.min.css') }}">
 
 		<link rel="stylesheet" href="{{ secure_asset('admin_assets/css/custom.css') }}">
 	</head>
@@ -77,11 +77,11 @@
 		</div>
 		<!-- ./wrapper -->
 		<!-- jQuery -->
-		<script src="{{ asset('admin_assets/plugins/jquery/jquery.min.js') }}"></script>
+		<script src="{{ secure_asset('admin_assets/plugins/jquery/jquery.min.js') }}"></script>
 		<!-- Bootstrap 4 -->
-		<script src="{{ asset('admin_assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+		<script src="{{ secure_asset('admin_assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 		<!-- AdminLTE App -->
-		<script src="{{ asset('/admin_assets/js/adminlte.min.js') }}"></script>
+		<script src="{{ secure_asset('/admin_assets/js/adminlte.min.js') }}"></script>
 		<!-- AdminLTE for demo purposes -->
 		{{-- <script src="js/demo.js"></script> --}}
 	</body>
