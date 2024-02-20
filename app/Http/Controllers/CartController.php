328049510f38f61f90c45c900070738fdb46a59a
richa <aliado.richa@gmail.com>
2024-02-20 17:01:30 +0800

    checkout

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Country;
 use App\Models\Product;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
@@ -123,8 +125,32 @@ class CartController extends Controller
         session()->flash('error', $message);
 
         return response()->json([
-            'status' => false,
+            'status' => true,
             'message' => $message
+        ]);
+    }
+
+    public function checkout() {
+        //-- if cart empty, redirect to cart page
+        if(Cart::count() == 0) {
+            return redirect()->route('front.cart');
+        }
+
+        //-- if user not logged in, redirect to login page
+        if(Auth::check() == false) {
+
+            if(!session()->has('url.intended')){
+                session(['url.intended' => url()->current()]);
+            }
+
+            return redirect()->route('account.login');
+        }
+
+        session()->forget('url.intended');
+
+        $countries = Country::orderBy('name', 'ASC')->get();
+        return view('front.checkout', [
+            'countries' => $countries
         ]);
     }
 }
