45cab5024af06c1eb3bf4bf657a5107312e7bdc0
richa <aliado.richa@gmail.com>
2024-02-21 23:13:08 +0800

    corrected misspell in message blade

--- resources/views/admin/message.blade.php
+++ resources/views/admin/message.blade.php
@@ -5,7 +5,7 @@
 </div>
 @endif
 
-@if (Session::has('sucess'))
+@if (Session::has('success'))
 <div class="alert alert-success alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <h4><i class="icon fa fa-check"></i> Success!</h4> {{ Session::get('success') }}
