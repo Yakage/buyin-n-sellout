9e42bb28fa88e92247f36fc0f6b82f80006f8b3d
Mica-Ela <mica.delacruz.up@phinmaed.com>
2024-02-22 23:38:13 +0800

    show ratings

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -82,9 +82,14 @@ class ShopController extends Controller
         return view('front.shop', $data);
     }
 
+
     public function product($slug) {
-        $product = Product::where('slug', $slug)->with('product_images')->first();
-        if($product == null) {
+        $product = Product::where('slug', $slug)
+            ->withCount('product_ratings')
+            ->withSum('product_ratings','rating')
+            ->with(['product_images','product_ratings'])->first();
+       
+        if($product == null) { 
             abort(404);
         }
 
@@ -97,6 +102,16 @@ class ShopController extends Controller
 
         $data['product'] = $product;
         $data['relatedProducts'] = $relatedProducts;
+        
+        //rating calculations
+        $avgRating = '0.00';
+        $avgRatingPer = '0';
+        if($product->product_ratings_count > 0) {
+            $avgRating = number_format(($product->product_ratings_sum_rating/$product->product_ratings_count),2);
+            $avgRatingPer = ($avgRating * 100)/5; 
+        }
+        $data['avgRating'] = $avgRating;
+        $data['avgRatingPer'] = $avgRatingPer;
         return view('front.product', $data);
     }
 
