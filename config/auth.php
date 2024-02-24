ea7e98e3080218e41b4a9c7486c9047f2676f044
s3e0o <143053347+s3e0o@users.noreply.github.com>
2024-02-24 16:06:16 +0800

    added admin seeder

--- config/auth.php
+++ config/auth.php
@@ -71,7 +71,7 @@ return [
 
         'admin' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Admin::class,
         ],
 
         // 'users' => [
