3fc92d954899913420d83f896ad265bcd9182045
s3e0o <143053347+s3e0o@users.noreply.github.com>
2024-02-27 11:35:53 +0800

    admin ;;

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,9 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'admin/login',
+        '/login',
         '/register',
-        '/admin/authenticate',
-        '/admin/dashboard'
+        '/authenticate',
+        '/dashboard'
     ];
 }
