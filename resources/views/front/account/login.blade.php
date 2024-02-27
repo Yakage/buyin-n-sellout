91d0458c869bf67a516c2b5a953863f704c5117d
s3e0o <143053347+s3e0o@users.noreply.github.com>
2024-02-27 15:07:31 +0800

    account login and register

--- resources/views/front/account/login.blade.php
+++ resources/views/front/account/login.blade.php
@@ -34,7 +34,7 @@
                 </div> 
                 <input type="submit" class="btn btn-dark btn-block btn-lg" value="Login">              
             </form>			
-            <div class="text-center small">Don't have an account? <a href="{{ route('front.account.register') }}">Sign up</a></div>
+            <div class="text-center small">Don't have an account? <a href="{{ route('account.register') }}">Sign up</a></div>
         </div>
     </div>
 </section>
