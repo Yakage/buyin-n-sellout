a0b6827d08009ce5137fc5b4ee42d2251f2c7daa
s3e0o <143053347+s3e0o@users.noreply.github.com>
2024-02-27 12:12:17 +0800

    admin login redo

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -7,10 +7,10 @@
 		<!-- Google Font: Source Sans Pro -->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 		<!-- Font Awesome -->
-		<link rel="stylesheet" href="{{ asset('admin_assets/plugins/fontawesome-free/css/all.min.css') }}">
+		<link rel="stylesheet" href="{{ secure_asset('admin_assets/plugins/fontawesome-free/css/all.min.css') }}">
 		<!-- Theme style -->
-		<link rel="stylesheet" href="{{ asset('admin_assets/css/adminlte.min.css') }}">
-		<link rel="stylesheet" href="{{ asset('admin_assets/css/custom.css') }}">
+		<link rel="stylesheet" href="{{ secure_asset('admin_assets/css/adminlte.min.css') }}">
+		<link rel="stylesheet" href="{{ secure_asset('admin_assets/css/custom.css') }}">
 	</head>
 	<body class="hold-transition login-page">
 		<div class="login-box">
@@ -71,11 +71,11 @@
 		</div>
 		<!-- ./wrapper -->
 		<!-- jQuery -->
-		<script src="{{ asset('admin_assets/plugins/jquery/jquery.min.js') }}"></script>
+		<script src="{{ secure_asset('admin_assets/plugins/jquery/jquery.min.js') }}"></script>
 		<!-- Bootstrap 4 -->
-		<script src="{{ asset('admin_assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+		<script src="{{ secure_asset('admin_assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 		<!-- AdminLTE App -->
-		<script src="{{ asset('admin_assets/js/adminlte.min.js') }}"></script>
+		<script src="{{ secure_asset('admin_assets/js/adminlte.min.js') }}"></script>
 		<!-- AdminLTE for demo purposes -->
 		{{-- <script src="{{ asset('admin_assets/js/demo.js') }}"></script> --}}
 	</body>
