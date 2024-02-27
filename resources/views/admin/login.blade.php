c675800521d3e00a210214d2af34a28c99f9e3d5
s3e0o <143053347+s3e0o@users.noreply.github.com>
2024-02-27 12:41:24 +0800

    modified login blade

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -24,7 +24,7 @@
 					<form action="{{ route('admin.authenticate') }}" method="post">
 						@csrf
 				  		<div class="input-group mb-3">
-							<input type="email" name="email" value="{{old('email')}}" id="password" class="form-control @error('email') is-invalid @enderror " placeholder="Email">
+							<input type="email" name="email" value="{{old('email')}}" id="email" class="form-control @error('email') is-invalid @enderror " placeholder="Email">
 							<div class="input-group-append">
 					  			<div class="input-group-text">
 									<span class="fas fa-envelope"></span>
