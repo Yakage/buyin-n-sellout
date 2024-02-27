3a1b5d65e6818fd93b518df444fd050cdc454671
s3e0o <143053347+s3e0o@users.noreply.github.com>
2024-02-27 12:28:56 +0800

    modified users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->integer('role');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
