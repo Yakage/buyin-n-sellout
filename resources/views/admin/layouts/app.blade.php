62c8d196948aab7fb08cfa2cf3238f842c3f8a7c
s3e0o <143053347+s3e0o@users.noreply.github.com>
2024-02-28 12:09:45 +0800

    admin blade mods

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -51,8 +51,12 @@
 							<img src="{{ asset('admin_assets/img/avatar5.png')}}" class='img-circle elevation-2' width="40" height="40" alt="">
 						</a>
 						<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right p-3">
-							<h4 class="h4 mb-0"><strong> {{ Auth::user()->name }} </strong></h4>
-							<div class="mb-3">{{ Auth::user()->email }} </div>
+							@if(Auth::check())
+								<h4 class="h4 mb-0"><strong>{{ Auth::user()->name }}</strong></h4>
+								<div class="mb-3">{{ Auth::user()->email }}</div>
+							@endif
+							{{-- <h4 class="h4 mb-0"><strong> {{ Auth::user()->name }} </strong></h4>
+							<div class="mb-3">{{ Auth::user()->email }} </div> --}}
 							<div class="dropdown-divider"></div>
 							<a href="#" class="dropdown-item">
 								<i class="fas fa-user-cog mr-2"></i> Settings								
