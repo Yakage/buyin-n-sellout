d22b8a9c5ff99c40b287b41384ed70d574bf0567
KristelJeanneB <krid.bautista.up@phinmaed.com>
2024-02-29 14:53:54 +0800

    routes

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -60,7 +60,7 @@
 							{{-- <h4 class="h4 mb-0"><strong> {{ Auth::user()->name }} </strong></h4>
 							<div class="mb-3">{{ Auth::user()->email }} </div> --}}
 							<div class="dropdown-divider"></div>
-							<a href="#" class="dropdown-item">
+							<a href="{{route('admin.processChangePassword')}}" class="dropdown-item">
 								<i class="fas fa-user-cog mr-2"></i> Settings								
 							</a>
 							<div class="dropdown-divider"></div>
