49d69f25aa843506502082c23e1b9f59e716cf05
s3e0o <143053347+s3e0o@users.noreply.github.com>
2024-02-29 08:45:49 +0800

    tempimage

--- app/Http/Controllers/web/admin/TempImagesController.php
+++ app/Http/Controllers/web/admin/TempImagesController.php
@@ -26,9 +26,9 @@ class TempImagesController extends Controller
                 'status' => true,
                 'name' => $newFileName,
                 'id' => $tempImage->id,
-                'image_id' => $tempImage->id,
-                'ImagePath' => asset('/temp/thumb/'.$newFileName),
-                'message' => 'Image uploaded successfully'
+                //'image_id' => $tempImage->id,
+                // 'ImagePath' => asset('/temp/thumb/'.$newFileName),
+                // 'message' => 'Image uploaded successfully'
             ]);
         }
 
