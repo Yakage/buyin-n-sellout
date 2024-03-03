64d2da7a7d0d72791c4f148448e71593c2fc5af2
s3e0o <143053347+s3e0o@users.noreply.github.com>
2024-03-03 20:19:24 +0800

    #registrationForm

--- resources/views/front/account/register.blade.php
+++ resources/views/front/account/register.blade.php
@@ -15,7 +15,7 @@
 <section class=" section-10">
     <div class="container">
         <div class="login-form">    
-            <form action="" method="post" name="registrationForm" id="registrationForm">
+            <form action="{{ route('account.processRegister') }}" method="post" name="registrationForm" id="registrationForm">
                 @csrf
                 <h4 class="modal-title">Register Now</h4>
                 <div class="form-group">
@@ -51,7 +51,7 @@
 
 @section('customJs')
     <script type="text/javascript">
-    $("$registrationForm").submit(function(event) {
+    $("#registrationForm").submit(function(event) {
         event.preventDefault();
 
         $("button[type='submit']").prop('disabled',true);
