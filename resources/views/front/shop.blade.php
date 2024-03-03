8656429445eb2ad415653777b733eb906c5b4ced
KristelJeanneB <krid.bautista.up@phinmaed.com>
2024-03-03 15:37:14 +0800

    updated cart

--- resources/views/front/shop.blade.php
+++ resources/views/front/shop.blade.php
@@ -118,7 +118,7 @@
                                     <div class="product-action">
                                         @if($product->track_qty == 'Yes')
                                             @if($product->qty > 0)
-                                            <a class="btn btn-dark" href="{{ route('front.addToCart') }}" onclick="addToCart({{ $product->id }});">
+                                            <a class="btn btn-dark" href="{{ route('front.addToCart') }}" onclick="cart({{ $product->id }});">
                                                 <i class="fa fa-shopping-cart"></i> Add To Cart
                                             </a>
                                             @else
@@ -127,7 +127,7 @@
                                             </a>
                                                 @endif
                                             @else
-                                            <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
+                                            <a class="btn btn-dark" href="{{route('front.cart')}}"  onclick="cart({{ $product->id }});">
                                                 <i class="fa fa-shopping-cart"></i> Add To Cart
                                             </a>
                                         @endif
