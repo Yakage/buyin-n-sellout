12c936ae1cf10c3f564146860d21ae1dddd35491
s3e0o <143053347+s3e0o@users.noreply.github.com>
2024-03-17 09:31:22 +0800

    checkout

--- database/migrations/2024_02_20_090318_create_orders_table.php
+++ database/migrations/2024_02_20_090318_create_orders_table.php
@@ -43,10 +43,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('orders');
-
-        // DB::statement('SET FOREIGN_KEY_CHECKS=0');
         // Schema::dropIfExists('orders');
-        // DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        Schema::dropIfExists('orders');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 };
